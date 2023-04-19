467d06550f491d40da4466f83f0ea6c543d94d88
GitHub Action <action@github.com>
2023-04-19 15:13:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/moba/peggyideapluggin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/moba/peggyideapluggin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.moba.peggyideapluggin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
